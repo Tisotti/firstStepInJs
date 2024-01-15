991b08875c95d2c9f8358c2d3897c3f797b67cd5
Tisotti <yana.albertivna@gmail.com>
2024-01-15 17:09:35 +0100

    tasks

---
+++ js/repeta-tasks.js
@@ -0,0 +1,14 @@
+// попадает ли число в отрезок 
+
+
+// const x1 = 1;
+// const x2 = 30;
+// const number = 5;
+
+// const result = number > x1 && number < x2;
+
+// console.log(result)
+
+
+// ---------------------------------------------
+
